1bbe6d6e74a9377f2a0819150d071eb17bb6d837
ESThing64 <89550738+ESThing64@users.noreply.github.com>
2021-10-24 16:37:59 -0500

    started working on the sever

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,5 +8,10 @@ VALUES ("Strategy"),
 
 
 INSERT INTO role_name (title, salary, department_id)
-VALUES ("Floor Manager", 600.00, 6),
-        ("Executive", 1000.00, 6);
+VALUES ("Floor Manager", 600.00),
+        ("Executive", 1000.00);
+
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Coakley"1, 1, 1);
