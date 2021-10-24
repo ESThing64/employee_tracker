db23cd8251ab72baff817262aa6a64551b8137c0
ESThing64 <89550738+ESThing64@users.noreply.github.com>
2021-10-24 15:06:52 -0500

    created a test db for departments

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO department (dep_name)
+VALUES ("Strategy"),
+        ("Marketing"),
+        ("Finance"),
+        ("Human resources"),
+        ("Technology and equipment"),
+        ("Operations");
+
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Manager", "60000.00");
